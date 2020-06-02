bc84c618884a7ff2ad0ed498a0095cbdb1d63faa
Your Name <you@example.com>
2020-06-02 20:14:43 +0800

    加入專案檔案。

---
+++ lotto.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm
+{
+    public partial class lotto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Convert.ToString(Session["account"]) == "000" && Convert.ToString(Session["password"]) == "111")
+            {
+                Response.Redirect("https://www.yahoo.com.tw");
+            }
+            else
+            {
+            }
+            //Random r = new Random();
+            //Label1.Text = $"{r.Next(1,48)},{r.Next(1, 48)},{r.Next(1, 48)},{r.Next(1, 48)},{r.Next(1, 48)},{r.Next(1, 48)}";
+            //Application.Lock();
+            //String test = Convert.ToString(Application["TEST"]);
+            //Response.Write(test);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Application.Lock();
+            //Application["TEST"] += ("<br/>" + account.Text);
+            //Application.UnLock();
+            //Response.Write(Application["TEST"]);
+            if (account.Text == "000" && password.Text == "111")
+            {
+                Session["account"] = "000";
+                Session["password"] = "111";
+                Response.Redirect("https://www.yahoo.com.tw");
+            }
+            else
+            {
+            }
+        }
+    }
+}
